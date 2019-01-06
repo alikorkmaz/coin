3e63bce40a49654310eaa860d64af01f2aecef81
Ali Korkmaz <ali@Alis-MacBook-Pro.local>
2019-01-06 23:47:57 +0300

    Updates

---
+++ precache-manifest.1f6914ce5f6b66ceadc2074fca50350b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39c7def497de524c6c21",
+    "url": "/coin/static/js/runtime~main.39c7def4.js"
+  },
+  {
+    "revision": "b07af39d1fbfa4cecb96",
+    "url": "/coin/static/js/main.b07af39d.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/coin/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "b07af39d1fbfa4cecb96",
+    "url": "/coin/static/css/main.2357c315.chunk.css"
+  },
+  {
+    "revision": "0add11fc2e6ca1c6c94d91a85cde5f7a",
+    "url": "/coin/index.html"
+  }
+];
