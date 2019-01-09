bec0a491e38a95ca534c6d3457b7de3d592f34e7
Ali Korkmaz <ali.korkmaz@ibtech.com.tr>
2019-01-09 17:28:34 +0300

    user agent added

--- src/App.js
+++ src/App.js
@@ -57,36 +57,30 @@ class App extends Component {
               coinbaseLtcAsk: jsonData.ask
             });
           });
-        fetch(
-          "https://cors-anywhere.herokuapp.com/https://www.bitstamp.net/api/v2/ticker/xrpeur/"
-        )
-          .then(res => res.json())
-          .then(jsonData => {
-            this.setState({
-              coinbaseXrpBid: jsonData.bid,
-              coinbaseXrpAsk: jsonData.ask
-            });
-          });
-
-        fetch("https://cors-anywhere.herokuapp.com/https://www.doviz.com").then(
-          res => {
-            res
-              .text()
-              .then(result => Parser(result))
-              .then(
-                result =>
-                  result.props.children[1].props.children[10].props.children[1]
-                    .props.children[1].props.children[1].props.children[3].props
-                    .children[3].props.children[1].props.children[3].props
-                    .children
-              )
-              .then(result => {
-                this.setState({
-                  guncelKur: result
-                });
+        let headers = new Headers({
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+        });
+        fetch("https://cors-anywhere.herokuapp.com/https://www.doviz.com", {
+          method: "GET",
+          headers: headers
+        }).then(res => {
+          res
+            .text()
+            .then(result => Parser(result))
+            .then(
+              result =>
+                result.props.children[1].props.children[10].props.children[1]
+                  .props.children[1].props.children[1].props.children[3].props
+                  .children[3].props.children[1].props.children[3].props
+                  .children
+            )
+            .then(result => {
+              this.setState({
+                guncelKur: result
               });
-          }
-        );
+            });
+        });
       });
   }
 
